1c2e4748dc55ff65a6c0ec4b5cc90cc5eeebf963
duynguyen <duyit192@hotmail.com>
2022-07-05 15:10:10 +0700

    create app content componet

---
+++ resources/views/components/app-content.blade.php
@@ -0,0 +1,10 @@
+<div class="app-content content">
+	<div class="content-overlay"></div>
+	<div class="header-navbar-shadow"></div>
+	<div class="content-wrapper">
+	    {{$cntHeader ?? NULL}}
+	    <div class="content-body">
+	    	{{$cntBody ?? NULL}}
+	    </div>
+	</div>
+</div>
